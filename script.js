c9c60e1eec18526cf26f61408fa79bd7dfb7c513
GurramAshish <141108968+GurramAshish@users.noreply.github.com>
2023-12-11 20:43:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log("Hello!! world");
+// Single line comment
+
+/* multiple 
+line
+comment
+*/
+
+
+
+// Arthematic operators
+let x=10;
+let y="Ashish";
+let z=30;
+console.log(x+z)
+console.log(x-z)
+console.log(x/z)
+console.log(x*z)
+console.log(x**z)
+console.log(x%z)
+console.log(x++)
+console.log(x--)
+console.log("THE ABOVE ARE ARTIMETIC OPERATORS")
+
+// DataTypes
+let num=20;
+let name="Ashish";
+
+
+// comparsion operators
+let num1=10;
+let num2="10"
+console.log(num1===num2);
+console.log(num1==num2);
+console.log(num1<=num2);
+console.log(num1>=num2);
+console.log(num1!=num2);
+console.log(num1<num2);
+console.log(num1>num2);
+console.log("THE ABOVE ARE COMPARISION OPERATORS")
